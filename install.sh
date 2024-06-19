3ef02110d5c8520bdd945fe27d60ac6766da661a
zyrethor <ian119912@gmail.com>
2024-06-19 13:27:22 +0800

    update sddm

--- install.sh
+++ install.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
 echo "Install packages..."
-sudo pacman -S tmux fish zoxide i3 xorg xorg-server alacritty kitty rofi polybar gedit thunar gvfs udiskie mpv thunar-volman xdg-user-dirs --noconfirm
+sudo pacman -S exa bat tmux fish zoxide i3 xorg xorg-server alacritty kitty rofi polybar gedit thunar gvfs udiskie mpv thunar-volman xdg-user-dirs --noconfirm
 
 yay -S lxappearance nordic-theme thunar-archive-plugin xarchiver unzip tumbler pulseaudio-control google-chrome ibus ibus-chewing pulseaudio pavucontrol flameshot feh --noconfirm
 
-yay -S audacious audacious-plugins audacity blueman bluetuith-bin calibre chezmoi gnome-keyring gpick grub2-theme-preview jdk-openjdk jdk17-openjdk jdk21-openjdk mousepad onlyoffice-bin openvpn picom pigz insync starship ttf-ligaconsolas-nerd-font visual-studio-code-bin xclip yt-dlp ffmpeg zsh zsh-autocomplete zsh-autosuggestions zsh-completions zsh-syntax-highlighting zsh-vi-mode qt5ct polychromatic libreoffice-fresh obs-studio typora typora eog font-manager gimp nordic-theme --noconfirm
+yay -S audacious audacious-plugins audacity blueman bluetuith-bin calibre chezmoi gnome-keyring gpick grub2-theme-preview jdk-openjdk jdk17-openjdk jdk21-openjdk mousepad onlyoffice-bin openvpn picom pigz insync starship ttf-ligaconsolas-nerd-font visual-studio-code-bin --noconfirm
+
+yay -S xclip yt-dlp ffmpeg zsh zsh-autocomplete zsh-autosuggestions zsh-completions zsh-syntax-highlighting zsh-vi-mode qt6ct polychromatic libreoffice-fresh obs-studio typora typora eog font-manager gimp nordic-theme --noconfirm
 
 yay -S noto-fonts-cjk ttf-ms-win11-auto ttf-blex-nerd-font-git ttf-jetbrains-mono-nerd papirus-icon-theme ttf-iosevka-nerd --noconfirm
 
-yay -S rxvt-unicode alsa-utils mate-power-manager nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts binutils gcc make pkg-config fakeroot ttf-font-awesome-5 --noconfirm
+yay -S kvantum rxvt-unicode alsa-utils mate-power-manager nitrogen rofi python-pip ttf-font-awesome adobe-source-code-pro-fonts binutils gcc make pkg-config fakeroot ttf-font-awesome-5 --noconfirm
 
-yay -S neofetch btop pywal-git calc mpd chrony networkmanager-dmenu-git --noconfirm
+yay -S fastfetch btop pywal-git calc mpd chrony networkmanager-dmenu-git --noconfirm
 
 yay -S git lazygit zoxide ripgrep sqlite fd yarn lldb nvm make unzip neovim python-pynvim --noconfirm
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd
+sudo mkdir -p /etc/X11/xorg.conf.d
+cd /etc/X11/xorg.conf.d
+sudo bash -c 'cat <<EOL > test.conf
+Section "InputClass"
+    Identifier "CapsLock to Control"
+    MatchIsKeyboard "on"
+    Option "XkbOptions" "ctrl:nocaps"
+EndSection
+EOL'
+cd
+
+
+bash -c 'cat <<EOL > /etc/environment
+#
+# This file is parsed by pam_env module
+#
+# Syntax: simple "KEY=VAL" pairs on separate lines
+#
+
+_JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
+QT_QPA_PLATFORMTHEME="qt6ct"
+EOL'
+
 
 cd
 git clone https://github.com/vinceliuice/grub2-themes.git
