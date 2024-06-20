593a116ab740ffe30d9f5f19b6281c64b4c051db
zyrethor <ian119912@gmail.com>
2024-06-21 00:48:33 +0800

    update theme

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 install_packages() {
     sudo pacman -S --noconfirm \
-        tree sddm exa bat tmux fish zoxide i3 xorg xorg-server alacritty kitty rofi polybar gedit thunar gvfs udiskie mpv thunar-volman xdg-user-dirs \
-        lxappearance thunar-archive-plugin xarchiver unzip tumbler ibus ibus-chewing pulseaudio pavucontrol flameshot feh \
+        tree sddm qt5‑graphicaleffects qt5‑quickcontrols2 qt5‑svg exa bat tmux fish zoxide i3 xorg xorg-server alacritty kitty rofi polybar gedit thunar gvfs udiskie mpv thunar-volman xdg-user-dirs \
+        lxappearance thunar-archive-plugin xarchiver unzip tumbler ibus ibus-chewing pulseaudio pavucontrol flameshot feh thefuck lxsession \
         audacious audacious-plugins audacity blueman bluetuith-bin calibre chezmoi gnome-keyring gpick jdk-openjdk jdk17-openjdk jdk21-openjdk mousepad openvpn picom pigz starship
 
     yay -S --noconfirm \
@@ -11,8 +11,9 @@ install_packages() {
         xclip yt-dlp ffmpeg zsh zsh-autosuggestions zsh-completions zsh-autocomplete zsh-syntax-highlighting zsh-vi-mode qt6ct polychromatic libreoffice-fresh obs-studio typora eog font-manager gimp \
         noto-fonts-cjk ttf-ms-win11-auto ttf-blex-nerd-font-git ttf-jetbrains-mono-nerd papirus-icon-theme ttf-iosevka-nerd ttf-ligaconsolas-nerd-font ttf-font-awesome adobe-source-code-pro-fonts ttf-font-awesome-5 \
         kvantum rxvt-unicode alsa-utils mate-power-manager nitrogen rofi python-pip binutils gcc make pkg-config fakeroot \
-        fastfetch btop pywal-git calc mpd chrony networkmanager-dmenu-git \
-        git lazygit zoxide ripgrep sqlite fd yarn lldb nvm make unzip neovim python-pynvim rustup
+        fastfetch btop pywal-git calc mpd chrony dmenu networkmanager-dmenu-git gdu evince ncmpcpp timidity++ \
+        fzf cmake ninja curl git lazygit zoxide ripgrep sqlite fd yarn lldb nvm make unzip python-pynvim rustup \
+        fcitx5 fcitx5-chewing fcitx5-chinese-addons fcitx5-configtool fcitx5-qt
 
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
@@ -20,9 +21,19 @@ install_packages() {
 }
 
 set_config() {
+    mkdir -p ~/.mpd/playlists
+
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+    omf install eclm
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
     sudo grub-mkfont -s 32 -o /boot/grub/fonts/iosevka_nerd_font.pcf /usr/share/fonts/TTF/IosevkaNerdFontMono-Regular.ttf
 
     cd ~
+    if [[ -d grub2-themes ]]; then
+        rm -rf grub2-themes
+    fi
     git clone https://github.com/vinceliuice/grub2-themes.git
     cd grub2-themes
     sudo ./install.sh -t stylish -s 2k
@@ -31,6 +42,9 @@ set_config() {
     cd ~
     if [[ -d /usr/share/sddm/themes/sugar-candy ]]; then
         sudo rm -rf /usr/share/sddm/themes/sugar-candy
+    fi
+    if [[ -d sddm-sugar-candy ]]; then
+        rm -rf sddm-sugar-candy
     fi
     sudo mkdir -p /usr/share/sddm/themes/sugar-candy
     git clone https://github.com/zyrethor/sddm-sugar-candy
@@ -71,11 +85,15 @@ EOL'
 
 
     systemctl --user enable pulseaudio
+    systemctl --user enable mpd
     sudo systemctl enable sddm.service
     sudo systemctl enable iwd
     sudo systemctl enable bluetooth.service
 
     xdg-user-dirs-update
+
+    chezmoi --init apply zyrethor
+    fc-cache -fv
 }
 
 
